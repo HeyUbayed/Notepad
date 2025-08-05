0b71875e4e87dcbffeec06a02e526b87005c1a1e
HeyUbayed <ubaid.8r@gmail.com>
2025-08-06 02:55:26 +0600

    Update Notepad.java
    Added keyboard shortcuts and a scrollbar.

--- Notepad/src/notepad/Notepad.java
+++ Notepad/src/notepad/Notepad.java
@@ -1,26 +1,32 @@
 package notepad;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class Notepad extends JFrame {
 
     private JTextArea area;
-    
+    private JScrollPane scpane;
+
     public Notepad() {
         setTitle("Notepad");
         setSize(1950, 1050);
         setLayout(new BorderLayout());
 
         area = new JTextArea();
-        area.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
         area.setLineWrap(true);
         area.setWrapStyleWord(true);
         
         JMenuBar menuBar = new JMenuBar(); 
+        menuBar.setBackground(Color.LIGHT_GRAY);
         
         JMenu file = new JMenu("File"); 
         JMenu edit = new JMenu("Edit");
+        
+        area.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
+        file.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        edit.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
         
         JMenuItem newdoc = new JMenuItem("New");
         JMenuItem open = new JMenuItem("Open");
@@ -32,6 +38,17 @@ public class Notepad extends JFrame {
         JMenuItem paste = new JMenuItem("Paste");
         JMenuItem cut = new JMenuItem("Cut");
         JMenuItem selectall = new JMenuItem("Select All");
+        
+        newdoc.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+        open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
+        save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+        print.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK));
+        exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0));
+        
+        copy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
+        paste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.CTRL_MASK));
+        cut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK));
+        selectall.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));               
         
         setJMenuBar(menuBar);
         menuBar.add(file);
@@ -47,8 +64,10 @@ public class Notepad extends JFrame {
         edit.add(paste);
         edit.add(cut);
         edit.add(selectall);
-        
-        add(area);
+           
+        scpane = new JScrollPane(area); 
+        add(scpane, BorderLayout.CENTER);
+        scpane.setBorder(BorderFactory.createLineBorder(Color.white, 7));
         setVisible(true);
     }
 
